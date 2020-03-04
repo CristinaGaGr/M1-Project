0544c0803199cbdc921c32a4dbc70dd225ffaca4
Cristina Garcia <cristinagarciagrillo@gmail.com>
2020-03-04 15:53:42 +0100

    Finished - except for search styles

--- js/search.js
+++ js/search.js
@@ -1,67 +1,31 @@
 //funciones para el search
-/*
-crear variable per seleccionar el input (input.value)
-*/
+
 let userInput = document.querySelector('.search-bar');
 
 let searchButton = document.querySelector('.search-btn');
 
 let allCards = document.querySelector('.all-cards');
 
-//TO DO:
-
-// Dins del .then (després de la meva variable del slice) un forEach i dins he de crear un article, afegir-li l'estructura amb backticks,
-// fer appenChild al meu "div all cards"
-//primer he d'accedir al div all cards //
-
 
 //Fetch API canciones
 
-// const myApi = () => {
-// 	fetch(`https://itunes.apple.com/search?limit=15&entity=musicTrack&term=${userInput.value}`)
-// 		.then(response => response.json())
-// 		.then(async (res) => {
-// 			let myResult = res.results;
-// 			myResult.forEach(e => {
-// 				let myResultName = e.trackName;
-// 				let myResultArtist = e.artistName;
-// 				let myResultPhoto = e.artworkUrl100;
-// 				let audio = e.previewUrl;
-// 				let artistUrl = e.artistViewUrl;
-// 				let genre = e.primaryGenreName;
-//
-// 				let newArticle = document.createElement('article');
-// 				newArticle.className = 'card';
-// 				newArticle.innerHTML = `
-// 				 <img class="random-image" src="${myResultPhoto}" alt="artist-photo"/>
-// 				 <div>
-//            			 <h3 class="song-title">${myResultName}</h3>
-//            			 <p class="genre">${genre}</p>
-//            			 <a target="_blank" href="${artistUrl}" class="artist">${myResultArtist}</a>
-//            		 </div>
-//            		 <audio controls>
-//                  	<source src="${audio}"/>
-//           		 </audio>
-// 				`;
-// 				allCards.appendChild(newArticle);
-// 			});
-// 		});
-// };
-//
-// searchButton.addEventListener('click', myApi);
-//
-let test = songs.slice(0, 15);
-test.forEach(e => {
-	let myResultName = e.trackName;
-	let myResultArtist = e.artistName;
-	let myResultPhoto = e.artworkUrl100;
-	let audio = e.previewUrl;
-	let artistUrl = e.artistViewUrl;
-	let genre = e.primaryGenreName;
+const myApi = () => {
+	allCards.innerHTML = '';
+	fetch(`https://itunes.apple.com/search?limit=15&entity=musicTrack&term=${userInput.value}`)
+		.then(response => response.json())
+		.then((res) => {
+			let myResult = res.results;
+			myResult.forEach(e => {
+				let myResultName = e.trackName;
+				let myResultArtist = e.artistName;
+				let myResultPhoto = e.artworkUrl100;
+				let audio = e.previewUrl;
+				let artistUrl = e.artistViewUrl;
+				let genre = e.primaryGenreName;
 
-	let newArticle = document.createElement('article');
-	newArticle.className = 'card';
-	newArticle.innerHTML = `
+				let newArticle = document.createElement('article');
+				newArticle.className = 'card';
+				newArticle.innerHTML = `
 				 <img class="random-image" src="${myResultPhoto}" alt="artist-photo"/>
 				 <div class="wrap-track-info">
            			 <h3 class="song-title">${myResultName}</h3>
@@ -76,32 +40,30 @@ test.forEach(e => {
           		 	<i class="far fa-pause-circle"></i>
 				</button>
 				`;
-	allCards.appendChild(newArticle);
-});
+				allCards.appendChild(newArticle);
+			});
+			let allPlayBtn = document.getElementsByClassName('play-music');
+			console.log(allPlayBtn);
+			Array.from(allPlayBtn).forEach((element) => {
+				element.addEventListener('click', playMusic);
 
+			});
 
-let allPlayBtn = document.getElementsByClassName('play-music');
-console.log(allPlayBtn);
+		})
+};
+
+searchButton.addEventListener('click', myApi);
 
 const playMusic = (event) => {
 	const currentElement = event.target;
 	const currentCard = event.target.parentNode.parentElement;
 	const currentAudio = currentCard.getElementsByTagName('audio')[0];
-	console.log(currentAudio);
-
-
 	if (currentElement.className.includes('fa-play')) {
 		currentAudio.play();
 	} else {
 		currentAudio.pause();
 	}
 };
-
-Array.from(allPlayBtn).forEach((element) => {
-	element.addEventListener('click', playMusic);
-
-
-});
 
 
 
